974bae694f4219596afd122c9326686fce88d719
Martin Ludwig <~>
2020-06-16 23:08:09 +0200

    Setup Godot and C# support with Visual Studio

---
+++ 79Nebula/Scripts/Button_Count_Increase.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class Button_Count_Increase : Button
+{
+	private int _counter = 0;
+	private Label _label;
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_label = GetNode<Label>("../Label_Counter");
+	}
+
+	private void _on_Button_Count_Increase_pressed()
+	{
+		_counter++;
+		_label.Text = _counter.ToString();
+	}
+}
