7ed030a9336c6234e61c5d6c8432475c40b4621e
Martin Ludwig <ML.Commits@gmail.com>
2020-08-06 20:42:50 +0200

    added BattleInstance

---
+++ 79Nebula/Models/BattleInstance.cs
@@ -0,0 +1,37 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nebula._79Nebula.Models
+{
+    class BattleInstance
+    {
+        private Player _player;
+        private List<Enemy> _enemies;
+
+        private AutoBattle autoBattle = new AutoBattle();
+
+        public BattleInstance(Player player, List<Enemy> enemies)
+        {
+            _player = player;
+            _enemies = enemies;
+        }
+
+        public void Battle()
+        {
+            Player opponent = SelectEnemy().ToPlayer();
+            autoBattle.Battle(_player, opponent);
+        }
+
+        private Enemy SelectEnemy()
+        {
+            Random random = new Random();
+            return _enemies.ElementAt(random.Next(0, _enemies.Count - 1));
+        }
+
+
+    }
+}
